fbbc0f73872985e2f76aed28a1bf85ce7e2bda70
yating <yating.liu@synsense.ai>
2022-10-26 16:55:59 +0800

    fourth homework commit

--- 4_homework_liuyating/img10_photo/img10_photo.py
+++ 4_homework_liuyating/img10_photo/img10_photo.py
@@ -1,12 +1,10 @@
 import cv2
-import numpy as np
 
 img = cv2.imread("image.png")
 
 for i in range(img.shape[0]):
     for j in range(img.shape[1]):
         b, g, r = img[i, j]
-        if b < 100 and g < 100 and r > 160:
+        if b < 92 and g < 92 and r > 167:
             img[i, j] = (237,149,100)
     cv2.imwrite('yy.png', img)
-
