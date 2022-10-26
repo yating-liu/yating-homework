254a7bab982379159c50851c8dc4a61c7ed4c83b
yating <yating.liu@synsense.ai>
2022-10-26 14:18:59 +0800

    second homework commit

--- 2_homework_liuyating/2_homework_liuyating.py
+++ 2_homework_liuyating/2_homework_liuyating.py
@@ -4,9 +4,9 @@ import numpy as np
 import pandas as pd
 import csv
 
-filepath = '/home/liuyating/Downloads/homework/20211228'
 
-def get_file_list(filepath, types=['.csv'], is_sort = True):
+
+def get_file_list(filepath, types=[], is_sort = True):
     file_list = []
     for path, dirs, files in os.walk(filepath):
         for file in files:
@@ -17,7 +17,7 @@ def get_file_list(filepath, types=['.csv'], is_sort = True):
         # print(file_list)
     return file_list
 
-# get_file_list(filepath, types=['.csv'])
+
 
 def parse_filename(filepath):
     filepath, suffix = os.path.splitext(filepath)
@@ -25,34 +25,71 @@ def parse_filename(filepath):
     label = label.split('_')[-1]
     return name, light_level, position, label
 
+def filter_file_list(file_list, lights = [], positions=[], names=[], labels=[]):
+    return list(filter(lambda x: filter_by_attributes(x, lights, positions, names, labels), file_list))
+
+def filter_by_attributes(filepath, lights = ['l01'], positions=[], names=[], labels=[]):
+    name, light_level, position, label = parse_filename(filepath)
+    if lights:
+        if light_level not in lights:
+            return False
+    if positions:
+        if position not in positions:
+            return False
+    if names:
+        if name not in names:
+            return False
+    if labels:
+        if label not in labels:
+            return False
+    return True
+
+def makedirs(filepath):
+    if not os.path.exists(filepath):
+        os.makedirs(filepath)
+
+def read_csv(filepath):
+    with open(filepath) as df:
+        myread = csv.reader(df)
+        mylist = myread.tolist()
+        return mylist
 
 
+def write_csvdata(filepath, myList):
+        with open(filepath) as cf:
+            mywrite = csv.writer(cf)
+            mywrite.writerow(myList)
 
-def calculate(data):
-    starTime = int(data[0])
-    endTime = int(data[1])
+
+def compute_csv_time(data):
+    rows = read_csv(filepath)
+    for row in rows:
+        row[1] - row[0]
+        classifacation
 
 
+def main():
+    filepath = '/home/liuyating/Downloads/homework/20211228'
+    file_list = get_file_list(filepath, types = ['.csv'], is_sort = True)
+    file_list = filter_file_list(file_list, lights = ['mylight'], positions=['myposition'], names=[], labels=['mylabel'])
+    mylight = ['l01']
+    myposition = ['p04']
+    mylabel = ['attentive', 'inattentive']
 
-# data1 = list(csv.reader(open('inattentive.csv')))
-# print(data1.ix[[0,1,3],:])
 
-# for i, row in enumerate(data[1:],1):
-#     data[i] = [int(elt) if elt.isdigit() else elt for elt in row]
-#     points = {} # an empty dictionary
-# for i, country in enumerate(data[0][2:],2):
-
-#新建文件
-# open("test.csv",'w').close()
-# #写入内容
-# with open("./test.csv", 'a') as cf:
-#     write = csv.writer(cf)
-#     write.writerow(['名字', '0', '2', '3', '4', '5'])
-#     data =  [str(person), str(0_time), str(2_time), str(3_time), str(4_time), str(4_time)]
-#     for i in data:
-#         write.writerow(i)
+    for attentive_file in file_list:
+        name, light_level, position, label = parse_filename(filepath)
+        class0, class2, class3, class4, class5 = 0 , 0 , 0 , 0 , 0
+        inattentive_file = filepath.replace('attentive', 'inattentive')
+        class0 = compute_csv_time(attentive_file)
+        class2, class3, class4, class5 = compute_csv_time(inattentive_file)
+        row = ','.join(name,class0,class2, class3, class4, class5 )
+        write_csvdata(result_file, row)
 
 
 
 if __name__ == '__main__':
     main()
+
+
+
